de4e90ccec9b0c2da742f59079f40d9f96427c11
Soo-Min Yoo <soomin.yoo95@gmail.com>
2015-04-11 13:40:27 -0700

    Add code on floats and doubles.

--- cs161/assign01.cpp
+++ cs161/assign01.cpp
@@ -12,17 +12,25 @@
 #include <cmath>
 #include <cfloat>
 
+/*********************************************
+ ** Function: 
+ ** Description: 
+ ** Parameters: 
+ ** Pre-Conditions: 
+ ** Post-Conditions: 
+ ********************************************/
 int main() {
 	std::cout << "Assignment 1: All About Numbers in C++\n\n";
 
 	std::cout << "Sizes of primitive types:" << std::endl;
-	std::cout << sizeof(int) << std::endl;
-	std::cout << sizeof(short) << std::endl;
-	std::cout << sizeof(long) << std::endl;
-	std::cout << sizeof(float) << std::endl;
-	std::cout << sizeof(double) << std::endl;
-	std::cout << sizeof(bool) << std::endl;
-	std::cout << sizeof(char) << std::endl;
+	std::cout << "int: "    << sizeof(int)    << std::endl;
+	std::cout << "short: "  << sizeof(short)  << std::endl;
+	std::cout << "long: "   << sizeof(long)   << std::endl;
+	std::cout << "float: "  << sizeof(float)  << std::endl;
+	std::cout << "double: " << sizeof(double) << std::endl;
+	std::cout << "bool: "   << sizeof(bool)   << std::endl;
+	std::cout << "char: "   << sizeof(char)   << std::endl;
+	std::cout << std::endl;
 
 	int sInt = INT_MIN;
 	int lInt = INT_MAX;
@@ -38,65 +46,60 @@ int main() {
 	unsigned long int lUlong = ULONG_MAX;
 
 	std::cout << "Values of macros:" << std::endl;
-	std::cout << sInt << std::endl;
-	std::cout << lInt << std::endl;
-	std::cout << sUint << std::endl;
-	std::cout << lUint << std::endl;
-	std::cout << sShort << std::endl;
-	std::cout << lShort << std::endl;
-	std::cout << sUshort << std::endl;
-	std::cout << lUshort << std::endl;
-	std::cout << sLong << std::endl;
-	std::cout << lLong << std::endl;
-	std::cout << sUlong << std::endl;
-	std::cout << lUlong << std::endl;
+	std::cout << "Smallest signed int: "     << sInt    << std::endl;
+	std::cout << "Largest signed int: "      << lInt    << std::endl;
+	std::cout << "Smallest unsigned int: "   << sUint   << std::endl;
+	std::cout << "Largest unsigned int: "    << lUint   << std::endl;
+	std::cout << "Smallest signed short: "   << sShort  << std::endl;
+	std::cout << "Largest signed short: "    << lShort  << std::endl;
+	std::cout << "Smallest unsigned short: " << sUshort << std::endl;
+	std::cout << "Largest unsigned short: "  << lUshort << std::endl;
+	std::cout << "Smallest signed long: "    << sLong   << std::endl;
+	std::cout << "Largest signed long: "     << lLong   << std::endl;
+	std::cout << "Smallest unsigned long: "  << sUlong  << std::endl;
+	std::cout << "Largest unsigned long: "   << lUlong  << std::endl;
+	std::cout << std::endl;
 
-	std::cout << "Values of variables:" << std::endl;
-	std::cout << sInt - 1 << std::endl;
-	std::cout << lInt + 1 << std::endl;
-	std::cout << sUint - 1 << std::endl;
-	std::cout << lUint + 1 << std::endl;
-	std::cout << sShort - 1 << std::endl;
-	std::cout << lShort + 1 << std::endl;
+	std::cout << "Values of variables in respective order:" << std::endl;
+	std::cout << sInt - 1    << std::endl;
+	std::cout << lInt + 1    << std::endl;
+	std::cout << sUint - 1   << std::endl;
+	std::cout << lUint + 1   << std::endl;
+	std::cout << sShort - 1  << std::endl;
+	std::cout << lShort + 1  << std::endl;
 	std::cout << sUshort - 1 << std::endl;
 	std::cout << lUshort + 1 << std::endl;
-	std::cout << sLong - 1 << std::endl;
-	std::cout << lLong + 1 << std::endl;
-	std::cout << sUlong - 1 << std::endl;
-	std::cout << lUlong + 1 << std::endl;
+	std::cout << sLong - 1   << std::endl;
+	std::cout << lLong + 1   << std::endl;
+	std::cout << sUlong - 1  << std::endl;
+	std::cout << lUlong + 1  << std::endl;
+	std::cout << std::endl;
 
-	std::cout << "Largest/smallest short, int, and long:" << std::endl;
-	std::cout << "Largest unsigned short: "  << pow (2, (sizeof(unsigned short) * 8)) - 1 << std::endl;
-	std::cout << "Smallest unsigned short: " << 0 << std::endl;
-	std::cout << "Largest signed short: "    << 0 << std::endl;
-	std::cout << "Smallest signed short: "   << 0 << std::endl;
-	std::cout << "Largest unsigned int: "    << 0 << std::endl;
+	std::cout << "Largest/smallest of short, int, and long:" << std::endl;
+	std::cout << "Smallest signed int: "     << 0 - pow (2, (sizeof(int) * 8)) - 1 << std::endl;
+	std::cout << "Largest signed int: "      << 2 * pow (2, (sizeof(int) * 8) - 1) - 1 << std::endl;
 	std::cout << "Smallest unsigned int: "   << 0 << std::endl;
-	std::cout << "Largest signed int: "      << 0 << std::endl;
-	std::cout << "Smallest signed int: "     << 0 << std::endl;
-	std::cout << "Largest unsigned long: "   << 0 << std::endl;
+	std::cout << "Largest unsigned int: "    << pow (2, (sizeof(unsigned int) * 8)) - 1 << std::endl;
+	std::cout << "Smallest signed short: "   << 0 - pow (2, (sizeof(short) * 8)) - 1 << std::endl;
+	std::cout << "Largest signed short: "    << 2 * pow (2, (sizeof(short) * 8) - 1) - 1 << std::endl;
+	std::cout << "Smallest unsigned short: " << 0 << std::endl;
+	std::cout << "Largest unsigned short: "  << pow (2, (sizeof(unsigned short) * 8)) - 1  << std::endl;
+	std::cout << "Smallest signed long: "    << 0 - pow (2, (sizeof(long) * 8)) - 1 << std::endl;
+	std::cout << "Largest signed long: "     << 2 * pow (2, (sizeof(long) * 8) - 1) - 1 << std::endl;
 	std::cout << "Smallest unsigned long: "  << 0 << std::endl;
-	std::cout << "Largest signed long: "     << 0 << std::endl;
-	std::cout << "Smallest signed long: "    << 0 << std::endl;
+	std::cout << "Largest unsigned long: "   << pow (2, (sizeof(unsigned long) * 8)) - 1 << std::endl;
+	std::cout << std::endl;
 
-	std::cout << 0 - pow(2, (sizeof(sInt) * 8 - 1)) << std::endl;
-	std::cout << 2 * (pow(2, (sizeof(lInt) * 8 - 1) - 1)) << std::endl;
-	std::cout << 0 << std::endl;
-	std::cout << pow (2, (sizeof(lUint) * 8) - 1) << std::endl;
-	std::cout << 0 - pow(2, (sizeof(sShort) * 8 - 1)) << std::endl;
-	std::cout << 2 * (pow(2, (sizeof(lShort) * 8 - 1) - 1)) << std::endl;
-	std::cout << pow (2, (sizeof(lUshort) * 8) - 1) << std::endl;
-	std::cout << 0 - pow(2, (sizeof(sInt) * 8 - 1)) << std::endl;
-	std::cout << 2 * (pow(2, (sizeof(lInt) * 8 - 1) - 1)) << std::endl;
-	std::cout << pow (2, (sizeof(lUlong) * 8) - 1) << std::endl;
-
-	std::cout << "Floats and doubles:" << std::endl;
-	std::cout << FLT_MANT_DIG << std::endl;
-	std::cout << DBL_MANT_DIG << std::endl;
-	std::cout << FLT_MIN_EXP << std::endl;
-	std::cout << FLT_MAX_EXP << std::endl;
-	std::cout << DBL_MIN_EXP << std::endl;
-	std::cout << DBL_MAX_EXP << std::endl;
+	std::cout << "Number of digits in mantissa: " << std::endl;
+	std::cout << "Float: "  << FLT_MANT_DIG << std::endl;
+	std::cout << "Double: " << DBL_MANT_DIG << std::endl;
+	std::cout << std::endl;
+	std::cout << "Largest and smallest exponent values: " << std::endl;
+	std::cout << "Float (smallest): "  << FLT_MIN_EXP << std::endl;
+	std::cout << "Float (largest): "   << FLT_MAX_EXP << std::endl;
+	std::cout << "Double (smallest): " << DBL_MIN_EXP << std::endl;
+	std::cout << "Double (largest): "  << DBL_MAX_EXP << std::endl;
+	std::cout << std::endl;
 
 	float fl;
 	double dbl;
@@ -110,8 +113,9 @@ int main() {
 	fl = fl - .01;
 	dbl = dbl - .01;
 
-	std::cout << fl << std::endl;
-	std::cout << dbl << std::endl;
+	std::cout << std::endl;
+	std::cout << "Entered float value - .01 = "  << fl << std::endl;
+	std::cout << "Entered double value - .01 = " << dbl << std::endl;
 
 
 	return 0;
