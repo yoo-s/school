c6df20dea841d2bb28ca39c7a0f9b4e5c69afb82
Soo-Min Yoo <soomin.yoo95@gmail.com>
2015-04-17 11:19:18 -0700

    Add assignment 2

---
+++ cs161/assign02.cpp
@@ -0,0 +1,27 @@
+/*********************************************
+ ** Program: assign01.cpp
+ ** Author: Soo-Min Yoo
+ ** Date: 04/10/15
+ ** Description: program exploring the different data types in C++ and their differing value ranges.
+ ** Input: none
+ ** Output: return 0
+ ********************************************/
+
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <cfloat>
+
+/*********************************************
+ ** Function: main
+ ** Description: prints data type sizes, macro values, and their min and max values.
+ ** Parameters: none
+ ** Pre-Conditions: none
+ ** Post-Conditions: return 0
+ ********************************************/
+int main() {
+
+	
+
+	return 0;
+}
