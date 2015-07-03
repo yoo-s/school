c967be57a4721b820e207b7f7c3bd508bdb19ecf
Soo-Min Yoo <soomin.yoo95@gmail.com>
2015-07-02 23:19:13 -0700

    Add comments

--- cs162/buttercup.cpp
+++ cs162/buttercup.cpp
@@ -20,22 +20,25 @@
  ** Pre-Conditions: none
  ** Post-Conditions: return 0
  ********************************************/
-
 int main() {
 	int suitors;
 
+	// Prompt user for number of suitors
 	std::cout << "How many suitors? ";
 	std::cin >> suitors;
 
+	// Initialize vector of input size, assigns each element with a number
 	std::vector<int> vect(suitors);
 	std::vector<int>::iterator it;
 	for (int i = 0; i < suitors; i++) {
 		vect.push_back(i+1);
 	}
+	// Prints out suitor numbers
 	for (int i = 0; i < suitors; i++) {
 		std::cout << vect[i] << " ";
 	}
 
+	// Loop through vector, eliminating every third element (suitor)
 	int index = 2;
 	while (vect.size() > 1) {
 		for (it = vect.begin(); it < vect.end(); it++) {
@@ -44,13 +47,14 @@ int main() {
 		}
 	}
 
+	// Prints remaining suitor number
 	std::cout << std::endl;
 	for (int i = 0; i < vect.size(); i++) {
 		std::cout << vect[i] << " ";
 	}
-
+	int winner = vect[0];
 	std::cout << std::endl;
-	//std::cout << "The winner would be suitor " << winner << std::endl;
+	std::cout << "The winner would be suitor " << winner << std::endl;
 
 	return 0;
 
