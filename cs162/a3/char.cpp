2ebc95a2cac53b372794c1cd460f8ea0c472658b
Soo-Min Yoo <soomin.yoo95@gmail.com>
2015-07-22 11:26:57 -0700

    Add options if else

--- cs162/a3/char.cpp
+++ cs162/a3/char.cpp
@@ -27,34 +27,6 @@ class Character {
 		virtual void set_armor() = 0;
 		virtual void set_strength() = 0;
 
-		virtual void round(Object& a, Object2& b) {			// how to do this??
-			std::cout << a.get_name() << " and " << b.get_name() << " fight!" << std::endl;
-			std::cout << "Fighting...\n" << std::endl;
-
-			while (a.strength() > 0 || b.strength() > 0) {
-				int atk1 = a.attack();
-				int dfs1 = a.defend();
-				int atk2 = b.attack();
-				int dfs2 = b.defend();
-				int dmg1 = atk2 - dfs1;
-				int dmg2 = atk1 - dfs2;
-				int app_dmg1 = dmg1 - a.armor();
-				int app_dmg2 = dmg2 - b.armor();
-				
-				a.strength() -= app_dmg1;
-				b.strength() -= app_dmg2;
-
-				if (a.strength() > b.strength()) {
-					winner = a.get_name();
-				} else {
-					winner = b.get_name();
-				}
-			}
-
-			std::cout << "The winner is: " << winner << "!" << std::endl;
-		}
-
-
 };
 
 #endif /* CHAR_CPP_ */
--- cs162/a3/test.cpp
+++ cs162/a3/test.cpp
@@ -6,13 +6,40 @@
 #include "char.cpp"
 #include "barbarian.cpp"
 
+void round(Object& a, Object2& b) {			// how to do this??
+	std::cout << a.get_name() << " and " << b.get_name() << " fight!" << std::endl;
+	std::cout << "Fighting...\n" << std::endl;
+
+	while (a.strength() > 0 || b.strength() > 0) {
+		int atk1 = a.attack();
+		int dfs1 = a.defend();
+		int atk2 = b.attack();
+		int dfs2 = b.defend();
+		int dmg1 = atk2 - dfs1;
+		int dmg2 = atk1 - dfs2;
+		int app_dmg1 = dmg1 - a.armor();
+		int app_dmg2 = dmg2 - b.armor();
+		
+		a.strength() -= app_dmg1;
+		b.strength() -= app_dmg2;
+
+		if (a.strength() > b.strength()) {
+			winner = a.get_name();
+		} else {
+			winner = b.get_name();
+		}
+	}
+
+	std::cout << "The winner is: " << winner << "!" << std::endl;
+}
+
 int main() {
 	srand(time(NULL));
 	
-	Barbarian *b1 = new Barbarian("Barbarian1");
-	Barbarian *b2 = new Barbarian("Barbarian2");
-	Character *c1 = b1;
-	Character *c2 = b2;
+	Barbarian *bb = new Barbarian("Barbarian1");
+	Barbarian *bb2 = new Barbarian("Barbarian2");
+	Character *c1 = bb;
+	Character *c2 = bb2;
 
 	std::string winner;
 	int option;
@@ -23,31 +50,39 @@ int main() {
 	std::cout << "Character 2: ";
 	std::cin >> option2;
 
-
-	std::cout << b1.get_name() << " and " << b2.get_name() << " fight!" << std::endl;
-	std::cout << "Fighting...\n" << std::endl;
-
-	while (b1.strength() > 0 || b2.strength() > 0) {
-		int atk1 = b1.attack();
-		int dfs1 = b1.defend();
-		int atk2 = b2.attack();
-		int dfs2 = b2.defend();
-		int dmg1 = atk2 - dfs1;
-		int dmg2 = atk1 - dfs2;
-		int app_dmg1 = dmg1 - b1.armor();
-		int app_dmg2 = dmg2 - b2.armor();
-		
-		b1.strength() -= app_dmg1;
-		b2.strength() -= app_dmg2;
-
-		if (b1.strength() > b2.strength()) {
-			winner = b1.get_name();
-		} else {
-			winner = b2.get_name();
-		}
+	if (option == 1 && option2 == 1) {
+		round(bb, bb2);
+/*	} else if (option == 1 && option2 == 2) {
+		round(bb, gb);
+	} else if (option == 1 && option2 == 3) {
+		round(bb, rp);
+	} else if (option == 1 && option2 == 4) {
+		round(bb, bm);
+	} else if (option == 1 && option2  == 5) {
+		round(bb, sh);
+	} else if (option == 2 && option2 == 2) {
+		round(gb, gb2);
+	} else if (option == 2 && option2 == 3) {
+		round(gb, rp);
+	} else if (option == 2 && option2 == 4) {
+		round(gb, bm);
+	} else if (option == 2 && option2 == 5) {
+		round(gb, sh);
+	} else if (option == 3 && option2 == 3) {
+		round(rp, rp2);
+	} else if (option == 3 && option2 == 4) {
+		round(rp, bm);
+	} else if (option == 3 && option2 == 5) {
+		round(rp, sh);
+	} else if (option == 4 && option2 == 4) {
+		round(bm, bm2);
+	} else if (option == 4 && option2 == 5) {
+		round(bm, sh);
+	} else if (option == 5 && option2 == 5) {
+		round(sh, sh2);*/
+	} else {
+		std::cout << "Not valid options, please try again." << std::endl;
 	}
-
-	std::cout << "The winner is: " << winner << "!" << std::endl;
 
 	return 0;
 
