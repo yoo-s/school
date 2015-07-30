98d004b38d97b42ae432a18b161439db322a56bd
Soo-Min Yoo <soomin.yoo95@gmail.com>
2015-07-30 01:03:10 -0700

    while loop and counter works, no place winners yet

--- cs162/a4/tournament.cpp
+++ cs162/a4/tournament.cpp
@@ -7,16 +7,17 @@
 #include "lineup.hpp"
 #include "losers.hpp"
 
-void round(Lineup &line1, Lineup &line2, Losers &pile) {
+void round(Lineup &line1, Lineup &line2, Losers &pile, int &s1, int &s2) {
 	int draw1;
 	int draw2;
 
 	char p1 = line1.getFront();
 	line1.removeFront(p1);
+	s1--;
 	char p2 = line2.getFront();
 	line2.removeFront(p2);
+	s2--;
 
-	std::cout << "Round:" << std::endl;
 	draw1 = (rand() % 10 + 1);
 	std::cout << "Roll 1: " << draw1 << std::endl;
 	draw2 = (rand() % 10 + 1);
@@ -25,17 +26,33 @@ void round(Lineup &line1, Lineup &line2, Losers &pile) {
 	if (draw1 > draw2) {
 		std::cout << "Player 1 wins!" << std::endl;
 		line1.addBack(p1);
+		s1++;
 		pile.push(p2);
 	} else if (draw1 < draw2) {
 		std::cout << "Player 2 wins!" << std::endl;
 		line2.addBack(p2);
+		s2++;
 		pile.push(p1);
 	} else if (draw1 == draw2) {
 		std::cout << "It's a draw!" << std::endl;
 		line1.addBack(p1);
+		s1++;
 		line2.addBack(p2);
+		s2++;
 	}
 	std::cout << std::endl;
+	std::cout << "Line 1: ";
+	line1.display();
+	std::cout << "\nLine 2: ";
+	line2.display();
+	std::cout << "\nLosers Pile: ";
+	pile.display();
+	std::cout << "\n" << std::endl;
+
+	std::cout << "Side 1 size: " << s1 << std::endl;
+	std::cout << "Side 2 size: " << s2 << std::endl;
+	std::cout << std::endl;
+
 
 }
 
@@ -45,22 +62,23 @@ int main() {
 	Lineup line1;
 	Lineup line2;
 	Losers pile;
-	int side1;
-	int side2;
+	int size1;
+	int size2;
 	char player;
 	char loser;
+	int roundnum = 1;
 
 	std::cout << "How many players on side 1? ";
-	std::cin >> side1;
+	std::cin >> size1;
 	std::cout << "Enter player names:" << std::endl;
-	for (int i = 0; i < side1; i++) {
+	for (int i = 0; i < size1; i++) {
 		std::cin >> player;
 		line1.addBack(player);
 	}
 	std::cout << "How many players on side 2? ";
-	std::cin >> side2;
+	std::cin >> size2;
 	std::cout << "Enter player names:" << std::endl;
-	for (int i = 0; i < side2; i++) {
+	for (int i = 0; i < size2; i++) {
 		std::cin >> player;
 		line2.addBack(player);
 	}
@@ -71,20 +89,18 @@ int main() {
 	line2.display();
 	std::cout << "\n" << std::endl;
 
-	round(line1, line2, pile);
+	/*std::cout << "Round " << roundnum << ":" << std::endl;
+	round(line1, line2, pile, side1, side2);
+	round(line1, line2, pile, side1, side2);
+	roundnum++;*/
+	while (size1 > 0 && size2 > 0) {
+		std::cout << "Round " << roundnum << "-------------" << std::endl;
+		round(line1, line2, pile, size1, size2);
+		roundnum++;
+	}
+
 
 	std::cout << "Final results:" << std::endl;
-	std::cout << "Line 1: ";
-	line1.display();
-	std::cout << "\nLine 2: ";
-	line2.display();
-	std::cout << "\nLosers Pile: ";
-	pile.display();
-	std::cout << std::endl;
-	/*while (line1.size() > 0 && line2.size() > 0) {
-	round(line1, line2, pile);
-	}*/
-
 	std::cout << "1st place winner: " << "a" << std::endl;
 	std::cout << "2nd place winner: " << "b" << std::endl;
 	std::cout << "3rd place winner: " << "c" << std::endl;
