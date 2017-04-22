6dbd67e1d895c713bce23caeaeecd67eb71b078a
Soo-Min Yoo <soomin.yoo95@gmail.com>
2017-04-22 16:53:40 -0700

    Add makefile and set up basic pthreads and noop functions

--- cs444/1/main.c
+++ cs444/1/main.c
@@ -1,4 +1,50 @@
+#include <pthread.h>
+#include <stdio.h>
+
+struct Item {
+	int first_value;
+	int second_value;
+};
+
+// When creating a thread using pthreads, you need to point it to a function
+// for it to start execution. This function is run by the producer thread.
+void *produce(void *item) {
+	return NULL;
+}
+
+void *consume(void *item) {
+	return NULL;
+}
+
+
 int main(int argc, char **argv) {
+	// create an item object
+	struct Item item;
+
+	// variable type pthread_t is a means of referencing threads
+	pthread_t producer;
+	pthread_t consumer;
+	
+	// create producer thread
+	if (pthread_create(&producer, NULL, produce, &item)) {
+		fprintf(stderr, "Unable to create thread\n");
+		return 1;
+	}
+	// create consumer thread
+	if (pthread_create(&consumer, NULL, consume, &item)) {
+		fprintf(stderr, "Unable to create thread\n");
+		return 1;
+	}
+
+	if(pthread_join(producer, NULL)) {
+		fprintf(stderr, "Unable to join thread\n");
+		return 2;
+	}
+	if(pthread_join(consumer, NULL)) {
+		fprintf(stderr, "Unable to join thread\n");
+		return 2;
+	}
+
 
 
 	return 0;
